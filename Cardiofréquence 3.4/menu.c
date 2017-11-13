0077ffdcb4e0208d7b8cd45eb09d58b1f1c87b0c
BenGround <benjaminhartmann.contact@gmail.com>
2017-11-13 16:18:58 +0100

    J'ai baisé ta mère

--- Cardiofréquence 3.4/menu.c
+++ Cardiofréquence 3.4/menu.c
@@ -8,10 +8,8 @@ int Menu_De_Base(int choix)
     int i;
     int choix2;
 
-    while(i!=0)
+    do
     {
-
-
     printf("Bienvenue dans la console ! \n\n");
     printf("Veuillez taper ce que vous voulez effectuer s'il vous plait ! \n\n");
     printf("1) Afficher les donnees dans l'ordre\n");
@@ -33,62 +31,68 @@ int Menu_De_Base(int choix)
     scanf("%d",&choix);
 
 
-
         switch(choix)
         {
             case 1:
+                printf("Vous avez choisi d'afficher les donnees dans l'ordre\n");
 
-                printf("Vous avez choisi l'affichage des donnees dans du fichier\n");
+
+                printf("\n\n");
                 break;
-
 
             case 2 :
 
                 printf("Vous avez choisi le tri est l'affichage par ordre croissant\n");
 
-
+                printf("\n\n");
                 break;
 
             case 3 :
                 printf("Vous avez choisi le tri est l'affichage par ordre decroissant\n");
-
+                printf("\n\n");
                 break;
             case 4 :
                 printf("Vous avez choisi l'affichage de la moyenne par unite de temps");
 
-
+                printf("\n\n");
                 break;
 
             case 5 :
                 printf("Vous avez choisi l'affichage du nombre de ligne de donnees actuellement en memoire");
 
-
+                printf("\n\n");
                 break;
 
             case 6 :
                 printf("Vous avez choisi l'affichage minimum de poul par unite de temps\n");
 
-
+                printf("\n\n");
                 break;
 
             case 7 :
                 printf("Vous avez choisi l'affichage maximum de poul par unite de temps\n");
 
-            break;
-
-
-
+                printf("\n\n");
+                break;
         }
-            printf("Vous voulez quitter la console, en etes vous sur ? \n \n");
-                printf("Veuillez taper 1 pour confirmer\n");
-                printf("Veuillez taper 2 pour retounez au menu\n");
-                scanf("%d", &choix2);
-                if (choix2 = 1)
-                    return EXIT_FAILURE;
-                else
-                    return Menu_De_Base(choix);
 
 
-    }
+
+    }while(choix!=0);
+
+        printf("Vous voulez quitter la console, en etes vous sur ? \n \n");
+        printf("Veuillez taper 1 pour confirmer\n");
+        printf("Veuillez taper 2 pour retounez au menu\n");
+
+        do{
+
+        scanf("%d", &choix2);
+        if(choix2 = 2){
+            Menu_De_Base(choix);
+            printf("\n");
+        }
+
+        }while(choix2 != 1);
+
     return 0;
 }
